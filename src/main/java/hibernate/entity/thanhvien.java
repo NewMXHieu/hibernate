ce7737274165461567dcab112e56b372fc065621
ChuViaD <131736749+ChuViaD@users.noreply.github.com>
2024-04-21 17:18:39 +0700

    Thống kê hoạt động thành viên

--- src/main/java/hibernate/entity/thanhvien.java
+++ src/main/java/hibernate/entity/thanhvien.java
@@ -2,25 +2,98 @@ package hibernate.entity;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "thanhvien")
 @Data
 public class thanhvien {
     @Id
+    @Column(name = "MaTV")
     private int MaTV;
-    @Column
+    @Column(name = "HoTen")
     private String HoTen;
-    @Column
+
+    @Column(name = "Khoa")
     private String Khoa;
-    @Column
+
+    @Column(name = "Nganh")
     private String Nganh;
-    @Column
-    private int SDT;
+
+    @Column(name = "SDT")
+    private String SDT;
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "thanhvien")
+    private List<xuly> xuly;
+
+    public thanhvien(int maTV, String hoTen, String khoa, String nganh, String sdt,String password, String email) {
+        this.MaTV = maTV;
+        this.HoTen = hoTen;
+        this.Khoa = khoa;
+        this.Nganh = nganh;
+        this.SDT = sdt;
+
+    }
+
+    public thanhvien() {
+
+    }
+
+    public int getMaTV() {
+        return MaTV;
+    }
+
+    public void setMaTV(int maTV) {
+        this.MaTV = maTV;
+    }
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.HoTen = hoTen;
+    }
+
+    public String getKhoa() {
+        return Khoa;
+    }
+
+    public void setKhoa(String khoa) {
+        this.Khoa = khoa;
+    }
+
+    public String getNganh() {
+        return Nganh;
+    }
+
+    public void setNganh(String nganh) {
+        this.Nganh = nganh;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String sdt) {
+        this.SDT = sdt;
+    }
+
+    public List<xuly> getXuly() {
+        return xuly;
+    }
+
+    public void setXuly(List<xuly> xuly) {
+        this.xuly = xuly;
+    }
+
+    @Override
+    public String toString() {
+        return this.HoTen;
+    }
+
+
+
 
 
 }
--- src/main/java/hibernate/entity/xuly.java
+++ src/main/java/hibernate/entity/xuly.java
@@ -7,34 +7,98 @@ package hibernate.entity;
 import javax.persistence.*;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "xuly")
 @Data
+@Getter
+@Setter
 public class xuly {
     @Id
+    @Column(name = "MaXL")
     private int MaXL;
 
-//    @ManyToOne(fetch = FetchType.EAGER)
-//    @JoinColumn(name = "MaTV", nullable = false,
-//            foreignKey = @ForeignKey(name = "xuly_ibfk_1"))
-    @Column
+    @Column (name = "MaTV")
     private int MaTV;
 
-    @Column
+    @Column (name = "HinhThucXL")
     private String HinhThucXL;
 
-    @Column
+    @Column (name = "SoTien")
     private Integer  SoTien;
 
-    @Column
+    @Column (name = "NgayXL")
     private Date NgayXL;
 
-    @Column
+    @Column (name = "TrangThaiXL")
     private int TrangThaiXL;
 
+    public xuly(int thanhvien, String HinhThucXL, Integer SoTien, Date NgayXL, int TrangThaiXL) {
+        this.MaTV = thanhvien;
+        this.HinhThucXL = HinhThucXL;
+        this.SoTien = SoTien;
+        this.NgayXL = NgayXL;
+        this.TrangThaiXL = TrangThaiXL;
+    }
+
+    public xuly() {
+
+    }
+
+    public int getMaXL() {
+        return MaXL;
+    }
+
+    public void setMaXL(int maXL) {
+        MaXL = maXL;
+    }
+
+    public int getMaTV() {
+        return MaTV;
+    }
+
+    public void setMaTV(int maTV) {
+        MaTV = maTV;
+    }
+
+    public String getHinhThucXL() {
+        return HinhThucXL;
+    }
+
+    public void setHinhThucXL(String hinhThucXL) {
+        HinhThucXL = hinhThucXL;
+    }
+
+    public Integer getSoTien() {
+        return SoTien;
+    }
+
+    public void setSoTien(Integer soTien) {
+        SoTien = soTien;
+    }
+
+    public Date getNgayXL() {
+        return NgayXL;
+    }
+
+    public void setNgayXL(Date ngayXL) {
+        NgayXL = ngayXL;
+    }
+
+    public int getTrangThaiXL() {
+        return TrangThaiXL;
+    }
+
+    public void setTrangThaiXL(int trangThaiXL) {
+        TrangThaiXL = trangThaiXL;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MaTV", referencedColumnName = "MaTV", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_thanhvien_handle"))
+    private thanhvien thanhvien;
 
 }
-
